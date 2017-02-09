c90a5207374240ea1950710d4de14e2788c6c540
Janis Vepris <janis.vepris@internations.org>
2017-02-09 15:17:16 +0200

    Scripts controller added.

---
+++ src/RoBundle/Controller/Api/ScriptsController.php
@@ -0,0 +1,26 @@
+<?php
+namespace RoBundle\Controller\Api;
+
+use ApiBundle\Controller\AbstractApiController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+class ScriptsController extends AbstractApiController
+{
+    /**
+     * @ApiDoc(
+     *     description="Get script page content by event id.",
+     *     section="Script",
+     *     tags={"TODO"}
+     * )
+     * @Rest\Get(
+     *     path="/api/events/{eventId}/script",
+     *     name="ro_api_scripts_get"
+     * )
+     * @Rest\View
+     */
+    public function getAction()
+    {
+        return $this->createView([]);
+    }
+}
