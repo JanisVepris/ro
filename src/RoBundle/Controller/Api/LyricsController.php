1d2ef0d5469abd15eeaafb5ff005594f0fc99d20
Janis Vepris <janis.vepris@internations.org>
2017-02-09 15:17:06 +0200

    Lyrics controller added.

---
+++ src/RoBundle/Controller/Api/LyricsController.php
@@ -0,0 +1,43 @@
+<?php
+namespace RoBundle\Controller\Api;
+
+use ApiBundle\Controller\AbstractApiController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+class LyricsController extends AbstractApiController
+{
+    /**
+     * @ApiDoc(
+     *     description="Get LyricItem list by event id.",
+     *     section="Lyrics",
+     *     tags={"TODO"}
+     * )
+     * @Rest\Get(
+     *     path="/api/events/{eventId}/lyrics",
+     *     name="ro_api_lyrics_index"
+     * )
+     * @Rest\View
+     */
+    public function indexAction()
+    {
+        return $this->createView([]);
+    }
+
+    /**
+     * @ApiDoc(
+     *     description="Get LyricItem by id.",
+     *     section="Lyrics",
+     *     tags={"TODO"}
+     * )
+     * @Rest\Get(
+     *     path="/api/events/{eventId}/lyrics/{lyricId}",
+     *     name="ro_api_lyrics_get"
+     * )
+     * @Rest\View
+     */
+    public function getAction()
+    {
+        return $this->createView([]);
+    }
+}
