f14b315b26ab4b0063fb73f1a9195541e9cefb2f
Janis Vepris <janis.vepris@internations.org>
2017-02-11 12:05:50 +0200

    UpdatedOn trait added

---
+++ src/CommonBundle/Traits/UpdatedOnEntityTrait.php
@@ -0,0 +1,35 @@
+<?php
+namespace CommonBundle\Traits;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+
+trait UpdatedOnEntityTrait
+{
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(name="updated_on", type="datetime")
+     */
+    protected $updatedOn;
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedOn()
+    {
+        return $this->updatedOn;
+    }
+
+    /**
+     * @param \DateTime $updatedOn
+     *
+     * @return $this
+     */
+    public function setUpdatedOn(\DateTime $updatedOn)
+    {
+        $this->updatedOn = $updatedOn;
+
+        return $this;
+    }
+}
