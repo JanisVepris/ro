cb5a3c96c65044484c053d74ad49eb1a2949b340
Janis Vepris <janis.vepris@internations.org>
2017-02-11 12:05:36 +0200

    CreatedOn trait added

---
+++ src/CommonBundle/Traits/CreatedOnEntityTrait.php
@@ -0,0 +1,34 @@
+<?php
+namespace CommonBundle\Traits;
+
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+
+trait CreatedOnEntityTrait
+{
+    /**
+     * @var \DateTime
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(name="created_on", type="datetime")
+     */
+    protected $createdOn;
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedOn()
+    {
+        return $this->createdOn;
+    }
+
+    /**
+     * @param \DateTime $createdOn
+     * @return $this
+     */
+    public function setCreatedOn(\DateTime $createdOn)
+    {
+        $this->createdOn = $createdOn;
+
+        return $this;
+    }
+}
