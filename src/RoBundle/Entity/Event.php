9c976e8668b53c63ce7d3c6613fe19cba9306bd0
Janis Vepris <janis.vepris@internations.org>
2017-02-11 12:36:31 +0200

    Event entity created

--- src/RoBundle/Entity/Event.php
+++ src/RoBundle/Entity/Event.php
@@ -2,16 +2,20 @@
 
 namespace RoBundle\Entity;
 
+use CommonBundle\Traits\UpdatedOnEntityTrait;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use CommonBundle\Traits\CreatedOnEntityTrait;
 
 /**
- * Event
- *
  * @ORM\Table(name="event")
  * @ORM\Entity(repositoryClass="RoBundle\Repository\EventRepository")
  */
 class Event
 {
+    use CreatedOnEntityTrait;
+    use UpdatedOnEntityTrait;
+
     /**
      * @var int
      *
@@ -31,7 +35,7 @@ class Event
     /**
      * @var \DateTime
      *
-     * @ORM\Column(name="eventDate", type="datetime", unique=true)
+     * @ORM\Column(name="event_date", type="datetime", unique=true)
      */
     private $eventDate;
 
@@ -42,6 +46,30 @@ class Event
      */
     private $image;
 
+    /**
+     * @var ArrayCollection|Article[]
+     * @ORM\OneToMany(targetEntity="RoBundle\Entity\Article", mappedBy="event")
+     */
+    private $articles;
+
+    /**
+     * @var Poster
+     * @ORM\OneToOne(targetEntity="RoBundle\Entity\Poster", mappedBy="event")
+     */
+    private $poster;
+
+    /**
+     * @var Team
+     * @ORM\OneToOne(targetEntity="RoBundle\Entity\Team", mappedBy="event")
+     */
+    private $team;
+    /**
+     * Event constructor.
+     */
+    public function __construct()
+    {
+        $this->articles = new ArrayCollection();
+    }
 
     /**
      * Get id
@@ -123,6 +151,63 @@ class Event
     public function getImage()
     {
         return $this->image;
+    }
+
+    /**
+     * @return ArrayCollection|Article[]
+     */
+    public function getArticles()
+    {
+        return $this->articles;
+    }
+
+    /**
+     * @param ArrayCollection|Article[] $articles
+     * @return Event
+     */
+    public function setArticles($articles)
+    {
+        $this->articles = $articles;
+
+        return $this;
+    }
+
+    /**
+     * @return Poster
+     */
+    public function getPoster()
+    {
+        return $this->poster;
+    }
+
+    /**
+     * @param Poster $poster
+     * @return Event
+     */
+    public function setPoster($poster)
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    /**
+     * @return Team
+     */
+    public function getTeam()
+    {
+        return $this->team;
+    }
+
+    /**
+     * @param Team $team
+     * @return Event
+     */
+    public function setTeam($team)
+    {
+        $this->team = $team;
+
+        return $this;
     }
 }
 
