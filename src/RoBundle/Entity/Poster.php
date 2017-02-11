0d4d34a4046cecd05d535c8bbb3d37028e464bfb
Janis Vepris <janis.vepris@internations.org>
2017-02-11 12:35:29 +0200

    Poster entity created

---
+++ src/RoBundle/Entity/Poster.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace RoBundle\Entity;
+
+use CommonBundle\Traits\CreatedOnEntityTrait;
+use CommonBundle\Traits\UpdatedOnEntityTrait;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Poster
+ *
+ * @ORM\Table(name="poster")
+ * @ORM\Entity(repositoryClass="RoBundle\Repository\PosterRepository")
+ */
+class Poster
+{
+    use CreatedOnEntityTrait;
+    use UpdatedOnEntityTrait;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255, nullable=true)
+     */
+    private $url;
+
+    /**
+     * @var Event
+     * @ORM\OneToOne(targetEntity="RoBundle\Entity\Event", inversedBy="poster")
+     * @ORM\JoinColumn(name="event_id", referencedColumnName="id")
+     */
+    private $event;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Poster
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return Event
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * @param Event $event
+     * @return Poster
+     */
+    public function setEvent($event)
+    {
+        $this->event = $event;
+
+        return $this;
+    }
+}
+
