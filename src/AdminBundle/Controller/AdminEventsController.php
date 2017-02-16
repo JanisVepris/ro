45ac519d36f044a2b60a22966ff770cfbc50ea8b
Janis Vepris <janis.vepris@internations.org>
2017-02-16 15:23:12 +0200

    Remove sonata bundles

---
+++ src/AdminBundle/Controller/AdminEventsController.php
@@ -0,0 +1,16 @@
+<?php
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminEventsController extends Controller
+{
+    /**
+     * @Route("/event/list", name="admin_events_list")
+     */
+    public function listAction()
+    {
+
+    }
+}
