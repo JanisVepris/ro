fbd2212d07eb815c38ae861badde58b30eb42b8f
Janis Vepris <janis.vepris@internations.org>
2017-02-25 12:38:48 +0200

    Define separate ckeditor configs

--- src/RoBundle/Form/Type/ArticleType.php
+++ src/RoBundle/Form/Type/ArticleType.php
@@ -16,7 +16,7 @@ class ArticleType extends AbstractType
         $builder
             ->add('title', TextType::class)
             ->add('description', TextareaType::class)
-            ->add('content', CKEditorType::class)
+            ->add('content', CKEditorType::class, ['config_name' => 'article_config'])
             ->add('published', CheckboxType::class, ['required' => false])
             ->add('articleImage', UploadFileType::class, ['data_class' => ArticleImage::class]);
     }
