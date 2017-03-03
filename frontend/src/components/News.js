743d90fba11c58b1b2e3b19354d6194219e5d7d8
Rokas Mikalkevicius <mikalkeviciusrokas@gmail.com>
2017-03-03 14:43:04 +0200

    Fix some of back navigation issues

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { loadEventNews } from '../actions/events'
+import { setHeaderCover } from '../actions/header'
 
 import NewsOverviewTable from '../containers/news/news-overview-table'
 
@@ -7,6 +8,7 @@ export default class App extends React.Component {
 
 	componentWillMount() {
 		this.props.dispatch(loadEventNews(this.props.eventId))
+		this.props.dispatch(setHeaderCover(this.props.cover))
 	}
 
 	render() {
--- frontend/src/containers/news.js
+++ frontend/src/containers/news.js
@@ -4,10 +4,12 @@ import NewsComponent from '../components/News'
 const mapStateToProps = (state) => {
 	
 	const activeEventId = state.app.activeEventId
+	const event = state.events.byId[activeEventId]
 	const news = state.events.newsById[activeEventId]
 	const eventName = state.app.eventsById[activeEventId].name
 
 	return {
+		cover: event && event.image, 
 		headerLoading: state.header.loading,
 		initialized: !!news,
 		eventId: activeEventId,
