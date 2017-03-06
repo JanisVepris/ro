d973096ccfde1fe2e601b99e41d3e4beb2b0e442
Rokas Mikalkevicius <mikalkeviciusrokas@gmail.com>
2017-03-06 23:06:17 +0200

    Some animations ant choppiness fixes

--- frontend/src/components/header/Popup.js
+++ frontend/src/components/header/Popup.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import OnClickOutside from 'react-onclickoutside'
-import SmoothCollapse from 'react-smooth-collapse'
+import Collapse from 'react-collapse'
 
 class Popup extends React.Component {
 
@@ -17,25 +17,24 @@ class Popup extends React.Component {
 		const className = 'header-dropdown' + (hidden ? ' hidden' : '')
 
 		return (
-				<div className={ className }>
-					<SmoothCollapse expanded={ !hidden }>
-					{ 
-						labels.map((label, index) => {
+			<div className={ className }>
+				<Collapse isOpened={ !hidden } >
+				{ 
+					labels.map((label, index) => {
 
-							const isSelected = index === selectedIndex
-							const selectedClassName = isSelected ? ' selected' : ''
-							const className = 'dropdown-option no-select pl-l'  + selectedClassName
+						const isSelected = index === selectedIndex
+						const selectedClassName = isSelected ? ' selected' : ''
+						const className = 'dropdown-option no-select pl-l'  + selectedClassName
 
-							return (
-								<div key={ index } className={ className } onClick={ actions[index] }>
-								{ label }
-							</div>
-							)
-						})
-					}
-					</SmoothCollapse>
-				</div>
-			
+						return (
+							<div key={ index } className={ className } onClick={ actions[index] }>
+							{ label }
+						</div>
+						)
+					})
+				}
+				</Collapse>
+			</div>
 		)
 	}
 
